2c536813a1afcca0e16049a4e47d4a0c80042518
Osei Rick <rickoseiampah@gmail.com>
2023-07-13 19:50:58 +0000

    logging improved

--- controllers/log_controller.js
+++ controllers/log_controller.js
@@ -18,7 +18,7 @@ const createEmployeeLogIn = async (req, res) => {
 };
 
 const createEmployeeLogOut = async (req, res) => {
-  const { employeeNumber: loggedNumber } = req.body;
+  const { employeeNumber: loggedNumber, date } = req.body;
   const employee = await EmployeeModel.findOne({
     employeeNumber: loggedNumber,
   });
@@ -26,6 +26,14 @@ const createEmployeeLogOut = async (req, res) => {
     throw new UnauthenticatedError("No employee with this number");
   }
   const { name } = employee;
+  const currentLog = await LogModel.findOne({
+    name,
+    date,
+    logType: "logged-in",
+  });
+  if (!currentLog) {
+    throw new UnauthenticatedError("Please log in before you log out");
+  }
   const tempLog = { ...req.body, name, logType: "logged-out" };
   const log = await LogModel.create(tempLog);
   res.status(StatusCodes.default.CREATED).json({ success: true, log });
@@ -33,6 +41,13 @@ const createEmployeeLogOut = async (req, res) => {
 
 const getAllLogs = async (req, res) => {
   const logs = await LogModel.find({});
+  res.status(StatusCodes.default.OK).json({ success: true, data: logs });
+};
+
+const getAllLogsForToday = async (req, res) => {
+  const { date: logDate } = req.params;
+
+  const logs = await LogModel.find({ date: logDate });
   res.status(StatusCodes.default.OK).json({ success: true, data: logs });
 };
 
@@ -46,5 +61,6 @@ module.exports = {
   createEmployeeLogIn,
   createEmployeeLogOut,
   getAllLogs,
+  getAllLogsForToday,
   deleteLog,
 };
--- routes/log_route.js
+++ routes/log_route.js
@@ -2,11 +2,13 @@ const {
   createEmployeeLogIn,
   createEmployeeLogOut,
   getAllLogs,
+  getAllLogsForToday,
   deleteLog,
 } = require("../controllers/log_controller");
 const router = require("express").Router();
 
 router.route("/").get(getAllLogs);
+router.route("/:date").get(getAllLogsForToday);
 router.route("/logged-in").post(createEmployeeLogIn);
 router.route("/logged-out").post(createEmployeeLogOut);
 router.route("/:id").delete(deleteLog);
