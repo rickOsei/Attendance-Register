469830b51d80c4806af9a6ad6500dc48a6f079b1
Osei Rick <rickoseiampah@gmail.com>
2023-07-14 23:24:47 +0000

    extra changes to log controller

--- controllers/log_controller.js
+++ controllers/log_controller.js
@@ -1,38 +1,72 @@
 const LogModel = require("../models/log_model");
 const EmployeeModel = require("../models/employee_model");
 const StatusCodes = require("http-status-codes");
-const { UnauthenticatedError } = require("../errors");
+const { UnauthenticatedError, BadRequestError } = require("../errors");
 
 const createEmployeeLogIn = async (req, res) => {
-  const { employeeNumber: loggedNumber } = req.body;
+  const { employeeNumber, date } = req.body;
+
+  // Ensuring employee number exists in our database
   const employee = await EmployeeModel.findOne({
-    employeeNumber: loggedNumber,
+    employeeNumber,
   });
+
   if (!employee) {
-    throw new UnauthenticatedError("No employee with this number");
+    throw new UnauthenticatedError(
+      `There is no employee associated with this number |${employeeNumber}|`
+    );
   }
+
+  // Preventing employees from logging more than once in a day
   const { name } = employee;
+  const alreadyLoggedIn = await LogModel.findOne({
+    employeeNumber,
+    date,
+    logType: "logged-in",
+  });
+  if (alreadyLoggedIn) {
+    throw new BadRequestError(
+      `The employee with number |${employeeNumber}| has already logged in today`
+    );
+  }
   const tempLog = { ...req.body, name, logType: "logged-in" };
   const log = await LogModel.create(tempLog);
   res.status(StatusCodes.default.CREATED).json({ success: true, log });
 };
 
 const createEmployeeLogOut = async (req, res) => {
-  const { employeeNumber: loggedNumber, date } = req.body;
+  const { employeeNumber, date } = req.body;
+
+  // Ensuring employee number exists in our database
   const employee = await EmployeeModel.findOne({
-    employeeNumber: loggedNumber,
+    employeeNumber,
   });
   if (!employee) {
-    throw new UnauthenticatedError("No employee with this number");
+    throw new UnauthenticatedError(
+      `There is no employee associated with this number |${employeeNumber}|`
+    );
   }
+
+  // Preventing employees from logging out without logging in and also logging out more than once
   const { name } = employee;
   const currentLog = await LogModel.findOne({
-    name,
+    employeeNumber,
     date,
     logType: "logged-in",
   });
+  const alreadyLoggedOut = await LogModel.findOne({
+    employeeNumber,
+    date,
+    logType: "logged-out",
+  });
   if (!currentLog) {
-    throw new UnauthenticatedError("Please log in before you log out");
+    throw new BadRequestError(
+      "Kindly ensure you log in for the day before logging out"
+    );
+  } else if (alreadyLoggedOut) {
+    throw new BadRequestError(
+      `The employee with number |${employeeNumber}| has already logged out today`
+    );
   }
   const tempLog = { ...req.body, name, logType: "logged-out" };
   const log = await LogModel.create(tempLog);
